06f3672f97b85b0be0e98907c59ef81a4e175725
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-06-30 15:14:20 -0400

    Create user.service.spec.ts

---
+++ FindHer/src/app/services/user.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { UserService } from './user.service';
+
+describe('Service: User', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UserService]
+    });
+  });
+
+  it('should ...', inject([UserService], (service: UserService) => {
+    expect(service).toBeTruthy();
+  }));
+});
