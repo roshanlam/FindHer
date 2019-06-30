4a818b5b11facec64136221965ba7f19e8bbb579
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2019-06-30 15:01:23 -0400

    Create app-routing.module.ts

---
+++ FindHer/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './guard/auth.guard';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { PreventUnsavedChangesGuard } from './guard/prevent-unsaved-changes.guard';
+import { MemberDetailResolver } from './resolvers/member-detail.resolver';
+import { ListsResolver } from './resolvers/lists.resolver';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailComponent, resolve: { aUserDetails: MemberDetailResolver } },
+      { path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard] },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'lists', component: ListsComponent,resolve: { userDetails: ListsResolver } }
+    ]
+  },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
